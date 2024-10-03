53e38b11d83cb847825e79116c97630019a27341
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-10-04 04:30:10 +0700

    refactor: careerCard

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,8 @@
-import {
-    ChevronRight,
-    Code,
-    ShoppingBag,
-    Users,
-    Briefcase,
-} from "lucide-react";
+import { Code, ShoppingBag, Users } from "lucide-react";
 import { Link } from "react-router-dom";
 import careersData from "../../assets/careers.json";
 import { useEffect, useState } from "react";
-
-const CareerCard = ({ career }: { career: any }) => {
-    console.log(career);
-    return (
-        <div className="flex flex-col space-y-3 bg-white p-6 rounded-lg shadow-md">
-            <Briefcase className="h-6 w-6" />
-            <h3 className="text-xl font-bold">{career?.jobTitle}</h3>
-            <p className="text-gray-500">
-                Lead complex tech projects and provide expert advice to our
-                clients.
-            </p>
-            <Link
-                to="#"
-                className="hover:underline mt-auto inline-flex items-center group"
-            >
-                Apply Now
-                <ChevronRight className="ml-1 h-4 w-4 transition-transform group-hover:translate-x-1" />
-            </Link>
-        </div>
-    );
-};
+import CareerCard from "../../components/CareerCard";
 
 export default function LandingPage() {
     const [careers, setCareers] = useState<any[]>([]);
