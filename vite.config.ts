4deb38ef7a9d7af8fc3b105cf99614650329fa57
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-10-06 03:22:40 +0700

    Update github-pages.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/koru/",
+  base: "/korutechnology.github.io/",
   plugins: [react()],
   resolve: {
     alias: {
